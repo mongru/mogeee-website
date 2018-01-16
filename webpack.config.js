556f05cd2906c8e53d2a45810e979d3c68b31a5f
mongru <monika.grubizna@gmail.com>
2018-01-16 11:52:29 +0100

    files structure

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: './src/js/app.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.min.js'
+        filename: 'bundle.js'
     },
     module: {
         rules: [
