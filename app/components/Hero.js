a9f0a8aff63e65d2ce3853fe61028bf951988385
mongru <monika.grubizna@gmail.com>
2018-03-09 00:04:41 +0000

    grid & media queries

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -10,13 +10,15 @@ class Hero extends React.Component {
           <p className="hero__text">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt </p>
         </div>
         <Carousel cellSpacing={20} cellAlign="center">
-          <div className="carousel__item"><img className="carousel__img" src="https://placeimg.com/1000/680/animals/grayscale" alt="" /></div>
-          <div className="carousel__item"><img className="carousel__img" src="https://placeimg.com/1000/680/people/grayscale" alt="" /></div>
+          <div className="carousel__item" />
+          <div className="carousel__item" />
+          <div className="carousel__item" />
+          {/* <div className="carousel__item"><img className="carousel__img" src="https://placeimg.com/1000/680/people/grayscale" alt="" /></div>
           <div className="carousel__item"><img className="carousel__img" src="https://placeimg.com/1000/680/tech/grayscale" alt="" /></div>
           <div className="carousel__item"><img className="carousel__img" src="https://placeimg.com/1000/680/any/grayscale" alt="" /></div>
           <div className="carousel__item"><img className="carousel__img" src="https://placeimg.com/1000/680/architecture/grayscale" alt="" /></div>
           <div className="carousel__item"><img className="carousel__img" src="https://placeimg.com/1000/680/nature/grayscale" alt="" /></div>
-          <div className="carousel__item"><img className="carousel__img" src="https://placeimg.com/1000/680/animals/grayscale" alt="" /></div>
+          <div className="carousel__item"><img className="carousel__img" src="https://placeimg.com/1000/680/animals/grayscale" alt="" /></div> */}
         </Carousel>
       </section>
     );
--- app/components/Work.js
+++ app/components/Work.js
@@ -4,21 +4,31 @@ import Draggable from 'react-draggable';
 class Work extends React.Component {
   render() {
     return (
-      <Draggable
-        axis="y"
-        handle=".handle"
-        defaultPosition={{ x: 0, y: 0 }}
-        position={null}
-        grid={[25, 25]}
-        onStart={this.handleStart}
-        onDrag={this.handleDrag}
-        onStop={this.handleStop}
-      >
-        <div>
-          <div>WORK</div>
-          <div className="handle" style={{ height: '300px', backgroundColor: 'turquoise' }}>Drag from here</div>
-        </div>
-      </Draggable>
+      <div>
+        <Draggable
+          axis="y"
+          handle=".handle"
+          defaultPosition={{ x: 0, y: 0 }}
+          position={null}
+          grid={[25, 25]}
+          onStart={this.handleStart}
+          onDrag={this.handleDrag}
+          onStop={this.handleStop}
+        >
+          <div>
+            <div>WORK</div>
+            <div className="handle" style={{ height: '300px', backgroundColor: 'turquoise' }}>Drag from here</div>
+          </div>
+        </Draggable>
+        <section className="gallery">
+          <div className="container">
+            <div className="row">
+              <div className="col-12">Hello img</div>
+            </div>
+          </div>
+          <div className="gallery__item">Gallery item</div>
+        </section>
+      </div>
     );
   }
 }
